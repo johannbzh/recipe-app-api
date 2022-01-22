9ebccb6fa4aceb7dc6b931fbada77319478aa148
johann bzaih <johann.bzaih@gmail.com>
2022-01-22 09:38:48 +0100

    Added user model with validations and tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@hello.com'
+        password = '12345'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email of a new user is normalized"""
+        email = 'test@helLLEO.com'
+        user = get_user_model().objects.create_user(email, '12345')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email=None, password='12345')
